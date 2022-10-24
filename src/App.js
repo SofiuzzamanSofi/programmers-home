aa175d51141edd0c62a69109c2cf62a311e061cd
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-10-25 02:18:38 +0600

    firebase beald file and md file changed

--- src/App.js
+++ src/App.js
@@ -3,31 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div >
-      <div>
-        <div className="mockup-window border bg-base-300">
-          <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
-        </div>
-      </div>
-      <div>
-        <div className="mockup-phone">
-          <div className="camera"></div>
-          <div className="display">
-            <div className="artboard artboard-demo phone-1">Hi.</div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content text-center">
-            <div className="max-w-md">
-              <h1 className="text-5xl font-bold">Hello there</h1>
-              <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-              <button className="btn btn-primary">Get Started</button>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      hell
     </div>
   );
 }
