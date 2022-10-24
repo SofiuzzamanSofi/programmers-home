99c7fb38c48a4d1958415a5bb7605f48a0d3eb83
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-10-25 02:06:19 +0600

    all check tailwind daisiUI react-router-dom heroicons title fabIcon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
+import { BeakerIcon } from '@heroicons/react/24/solid'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>hellow from sofi </p>
-      <h4>this is from programmers home </h4>
+    <div >
+      <div>
+        <div className="mockup-window border bg-base-300">
+          <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+        </div>
+      </div>
+      <div>
+        <div className="mockup-phone">
+          <div className="camera"></div>
+          <div className="display">
+            <div className="artboard artboard-demo phone-1">Hi.</div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <h1 className="text-5xl font-bold">Hello there</h1>
+              <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
